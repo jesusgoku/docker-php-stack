01c2d031f6cb11a501500035671479c1aea7ff09
Jesús Urrutia <jesus.urrutia@gmail.com>
2015-09-10 18:42:22 -0300

    Added options for Symfony and Silex application

--- README.md
+++ README.md
@@ -15,10 +15,16 @@ For build image execute:
 docker build -t image-name .
 ```
 
+Enviroment vars avaialables:
+
+- GIT_REPO (**Required**) Repository with the application
+- SYMFONY_APP (**Optional**) Set when your app is a Symfony2 application
+- SILEX_APP (**Optional**) Set when your app is a Silex application
+
 For run generate image, execute:
 
 ```sh
-docker run -it --rm -e GIT_REPO="http://github.com/public/php-application.git" -p 80:80 --name image-name container-name
+docker run -it --rm -e GIT_REPO="http://github.com/public/php-application.git" -p 80:80 -p 8080:8080 --name image-name container-name
 ```
 
 > **Important:** replace *GIT_REPO* with your application code hosted on GitHub
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,30 @@
 #!/usr/bin/env bash
 
 TTY_PASS=`pwgen -s 12 1` && sed -i.bak "s/PASSWORD/${TTY_PASS}/" /terminal/index.js
-# pm2 start /terminal/index.js -i 0 --name "terminal"
+
+if [ ! -z "$GIT_REPO" ]; then
+    if [ -z "${GIT_BRANCH}" ]; then
+        GIT_BRANCH=master
+    fi
+
+    rm -rf app
+    git clone --depth 1 --branch="${GIT_BRANCH}" "$GIT_REPO" app
+
+    cd app
+
+    if [ -f "composer.json" ]; then
+        composer install --prefer-dist --no-dev --optimize-autoloader
+    fi
+
+    if [ -f "bower.json" ]; then
+        bower install --production
+    fi
+
+    if [ ! -z "${SYMFONY_APP}" ] || [ ! -z "${SILEX_APP}" ]; then
+        echo "=> Is a SYMFONY or SILEX app. Change web root to web folder."
+        sed -i.bak 's/www\/html/www\/html\/web/' /etc/apache2/sites-available/000-default.conf
+    fi
+fi
 
 echo "========================================================================"
 echo "You can now connect to this TTY using:"
@@ -10,11 +33,5 @@ echo "=> Enter in your browser: https://host:8080"
 echo "=> Username: admin / Password: ${TTY_PASS}"
 echo ""
 echo "========================================================================"
-
-
-if [ ! -z "$GIT_REPO" ]; then
-    rm -rf app
-    git clone --depth 1 "$GIT_REPO" app
-fi
 
 /run.sh
